0b40e551c4ee80932daf0822a3aa5d019e514fee
Николай Сухих <nikolay.suhih@nutnet.ru>
2021-09-10 00:06:57 +0400

    Upgrade versions :bookmark:

--- docker/nginx/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for f in $(find /var/www/docker/nginx/conf.d/ -name *.conf.template); \
-	do envsubst '$APP_DOMAIN' < $f > "/etc/nginx/conf.d/$(basename $f .template)"; done
-nginx
-
